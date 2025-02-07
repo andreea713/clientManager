e539ef69f8bed4d602afd176f889df2a6542603b
andreea713 <andreea.drume@gmail.com>
2025-02-07 14:10:52 +0100

    Create ClientRepository

--- clientManager/src/main/java/com/portfolio/srv/Services.java
+++ clientManager/src/main/java/com/portfolio/srv/Services.java
@@ -2,10 +2,18 @@ package com.portfolio.srv;
 
 import com.portfolio.api.Client;
 import com.portfolio.api.ClientApi;
+import com.portfolio.repositories.ClientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
+@Service
 public class Services implements ClientApi {
+
+    private final ClientRepository clientRepository;
+
     @Override
     public void createClient(Client client) {
-
+        clientRepository.save(client);         // create a client in the repository
     }
 }
