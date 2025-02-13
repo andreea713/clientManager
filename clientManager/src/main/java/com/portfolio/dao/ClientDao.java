af714d19be3381a726bdd40d54e9065b40dee4fe
andreea713 <andreea.drume@gmail.com>
2025-02-13 18:31:38 +0100

    New files

---
+++ clientManager/src/main/java/com/portfolio/dao/ClientDao.java
@@ -0,0 +1,37 @@
+package com.portfolio.dao;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Table(name = "Client")
+public class ClientDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idClient", nullable = false, unique = true)
+    UUID idClient;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "surname")
+    String surname;
+
+    @Column(name = "number", nullable = false)
+    int number;
+
+    @Column(name = "email", nullable = false, unique = true)
+    String email;
+
+    @Column(name = "creationDate", updatable = false)
+    @CreationTimestamp
+    Instant creationDate;
+
+    @Column(name = "updateDate")
+    @UpdateTimestamp
+    Instant updateDate;
+}
