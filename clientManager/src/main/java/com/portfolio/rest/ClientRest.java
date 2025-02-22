675adee445004ee234df8f6a94f21200642f460b
andreea713 <andreea.drume@gmail.com>
2025-02-22 16:02:35 +0100

    log.info in 'ClientRest'

--- clientManager/src/main/java/com/portfolio/rest/ClientRest.java
+++ clientManager/src/main/java/com/portfolio/rest/ClientRest.java
@@ -3,32 +3,37 @@ package com.portfolio.rest;
 import com.portfolio.api.Client;
 import com.portfolio.api.ClientApi;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @AllArgsConstructor    // create constructor
+@Log4j2
 public class ClientRest {
     private final ClientApi clientApi;
 
     @PostMapping("/createClient")       // name of endpoint
     public void createClient(@RequestBody Client client){   // the request has something in the body
         clientApi.createClient(client);
+        log.info("New client created");
     }
 
     @GetMapping("/getClients")
     public List<Client> getClients(){
+        log.info("Getting clients: ... ");
         return clientApi.getClients();
     }
 
     @PutMapping("/updateClient")
     public void updateClient(@RequestBody Client client){
         clientApi.updateClient(client);
+        log.info("The client has been updated");
     }
 
     @DeleteMapping("/deleteClient/{idClient}")
     public void deleteClient(@PathVariable("idClient") String idClient){
         clientApi.deleteClient(idClient);
+        log.info("Client with ID: {} has been removed", idClient);
     }
 }
